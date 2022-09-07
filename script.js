bd07c6f927d75dccfe74269bad7e85cf136d092b
Ashim Poudel <poudelashim26@gmail.com>
2022-09-07 18:42:05 +1000

    final betting practise

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ const game = {
   ],
 
   score: '4.0',
-  socred: ['Lewandoski', 'Sane', 'Lewandoski', 'Hummels'],
+  scored: ['Lewandoski', 'Sane', 'Lewandoski', 'Hummels'],
   date: 'Nov 9th, 2037',
   odds: {
     team1: 1.33,
@@ -169,6 +169,34 @@ const game = {
   },
 };
 
-//1.
+//1. arrays destructuring
 const [players1, players2] = game.players;
 console.log(players1, players2);
+
+//2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+//3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+//4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+
+//5.
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+//6.
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored`);
+};
+printGoals(...game.scored);
+
+//7.
+team1 < team2 && console.log('Team 1 is more likely to win the game');
+team2 > team1 && console.log('Team2 is more likely to win the game');
